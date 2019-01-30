a001081b107371b642067eb0c51550854dfe305b
Michael Krug <michi.krug@gmail.com>
2019-01-31 00:38:44 +0100

    Minor changes

--- db/rmWrapper.py
+++ db/rmWrapper.py
@@ -1187,6 +1187,7 @@ class RmWrapper(DbWrapperBase):
                 "FROM pokestop inner join trs_quest on "
                 "pokestop.pokestop_id = trs_quest.GUID where "
                 "DATE(from_unixtime(trs_quest.quest_timestamp,'%Y-%m-%d')) = CURDATE()"
+                "ORDER BY trs_quest.quest_timestamp DESC"
             )
             data = ()
         else:

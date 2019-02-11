2e550eb1e5067ba45ba47ca97f92f835279a71c4
Michael Krug <michi.krug@gmail.com>
2019-02-11 21:08:08 +0100

    Code formatting

--- utils/status.py
+++ utils/status.py
@@ -1,8 +1,9 @@
-import logging
 import json
+import logging
 import os
 
 log = logging.getLogger(__name__)
+
 
 def set_status(origin, data):
     if os.path.exists('status.json'):
@@ -10,12 +11,12 @@ def set_status(origin, data):
             status = json.load(f)
     else:
         status = {}
-        
+
     if 'player' in status[origin]:
         player_stats = status[origin]['player']
-        
+
     status[origin] = data
-    status[origin]['player'] = player_stats 
+    status[origin]['player'] = player_stats
 
     with open('status.json', 'w') as outfile:
         json.dump(status, outfile, indent=4, sort_keys=True)

43badba437198412312cd576abe044801861ac8d
Michael Krug <michi.krug@gmail.com>
2019-03-06 00:17:23 +0100

    Fix wrong id

--- utils/webhookHelper.py
+++ utils/webhookHelper.py
@@ -457,7 +457,7 @@ class WebhookHelper(object):
                                       pokemon_level=None, cp_multiplier=None, form=None, cp=None,
                                       individual_attack=None, individual_defense=None, individual_stamina=None,
                                       move_1=None, move_2=None, height=None, weight=None, gender=None, boosted_weather=None):
-        log.info('Sending Pokemon %s (#%s) to webhook', pokemon_id, id)
+        log.info('Sending Pokemon %s (#%s) to webhook', pokemon_id, encounter_id)
 
         mon_payload = {"encounter_id": encounter_id, "pokemon_id": pokemon_id, "last_modified_time": last_modified_time,
                        "spawnpoint_id": spawnpoint_id, "latitude": lat, "longitude": lon,

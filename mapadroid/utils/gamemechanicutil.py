1871d4e868e50e42151325e10bdf9d0b1b433e00
Michael Krug <michi.krug@gmail.com>
2024-05-07 00:08:43 +0200

    Prevent exception when cp_multiplier is None

--- mapadroid/utils/gamemechanicutil.py
+++ mapadroid/utils/gamemechanicutil.py
@@ -10,6 +10,8 @@ import mapadroid.mitm_receiver.protos.Rpc_pb2 as pogoprotos
 
 
 def calculate_mon_level(cp_multiplier):
+    if cp_multiplier is None:
+        return 0
     if cp_multiplier < 0.734:
         pokemon_level = 58.35178527 * cp_multiplier * cp_multiplier - 2.838007664 * cp_multiplier + 0.8539209906
     else:
